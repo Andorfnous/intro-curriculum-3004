8fd6f8efef34e2f96211c2d630b07cba042d6587
いぶき <morikauki@gmail.com>
2022-02-22 00:43:12 +0900

    完成

--- sum/index.js
+++ sum/index.js
@@ -1,14 +1,19 @@
-'use strict';
+`use strict`;
 
-/**
- * 数値の配列を受け取って、その要素の合計を返す関数
- */
-function add(numbers) {
-  let result = 0;
-  for (const num of numbers) {
-    result = result + num;
-  }
-  return result;
+function add(numbers){
+    let result = 0;
+    for (const num of numbers){
+        result = result + num;
+    }
+    return result;
 }
 
-module.exports = { add };
+function multi(numbers){
+    let result = 1;
+    for (const num of numbers){
+        result = result * num;
+    }
+    return result;
+}
+
+module.exports = { add, multi };
